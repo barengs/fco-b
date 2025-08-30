f4b38d8982a6fe8a2447f54ff131a088037ec7cb
rofiareiv <kambing.rofi@gmail.com>
2025-08-30 12:11:00 +0700

    update endpoint auth

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register(r'auth', views.RegistrationViewSet, basename='auth')
+router.register(r'', views.RegistrationViewSet, basename='auth')
 
 urlpatterns = [
     path('', include(router.urls)),
